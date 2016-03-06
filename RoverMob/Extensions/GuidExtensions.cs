497fae5715f06caf0fc7b12bd23b3b92a102cf4a
Michael L Perry <michael@qedcode.com>
2016-03-06 15:08:17 -0600

    Generate a deterministic GUID for any object.

--- RoverMob/Extensions/GuidExtensions.cs
+++ RoverMob/Extensions/GuidExtensions.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.IO;
+using System;
+using System.IO;
 using System.Text.RegularExpressions;
 
 namespace RoverMob
@@ -10,6 +14,21 @@ namespace RoverMob
         public static string ToCanonicalString(this Guid guid)
         {
             return Punctuation.Replace(guid.ToString(), "").ToLower();
+        }
+
+        public static Guid ToGuid(this object document)
+        {
+            var sha = new Sha256Digest();
+            var stream = new DigestStream(new MemoryStream(), null, sha);
+            using (var writer = new StreamWriter(stream))
+            {
+                string mementoToString = JsonConvert.SerializeObject(document);
+                writer.Write(mementoToString);
+            }
+            byte[] buffer = new byte[sha.GetDigestSize()];
+            sha.DoFinal(buffer, 0);
+            Array.Resize(ref buffer, 16);
+            return new Guid(buffer);
         }
     }
 }
